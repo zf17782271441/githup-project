e55ef07beb8d6260cb1af47fd566e02db6e6bf21
zhoufei <956156085@qq.com>
2019-06-12 11:03:24 +0800

    1
    Signed-off-by: zhoufei <956156085@qq.com>

---
+++ 3104_xml_rollback.sql
@@ -0,0 +1,24 @@
+--delete from tbl_buf_dsp t where t.i_buf_dsp_index = '1237' and t.l_usage_key = '1';
+--delete from tbl_buf_dsp t where t.i_buf_dsp_index = '1238' and t.l_usage_key = '1';
+--delete from tbl_buf_dsp t where t.i_buf_dsp_index = '1239' and t.l_usage_key = '1';
+--delete from tbl_buf_dsp t where t.i_buf_dsp_index = '1240' and t.l_usage_key = '1';
+--delete from tbl_fld_tsf t where t.i_buf_chg_index = '1247';
+--delete from tbl_fld_tsf t where t.i_buf_chg_index = '1248';
+--delete from tbl_fld_tsf t where t.i_buf_chg_index = '1249';
+--delete from tbl_fld_tsf t where t.i_buf_chg_index = '1250';
+--delete from tbl_ipc_dft t where t.ipc_dft_index = '1237';
+--delete from tbl_ipc_dft t where t.ipc_dft_index = '1238';
+--delete from tbl_ipc_dft t where t.ipc_dft_index = '1239';
+--delete from tbl_ipc_dft t where t.ipc_dft_index = '1240';
+--delete from tbl_xml_vlues t where t.i_buf_dsp_index = '1179';
+--delete from tbl_xml_vlues t where t.i_buf_dsp_index = '1180';
+
+--delete from tbl_xml_trans_dsp t where t.txn_number = '61001511';
+
+--delete from tbl_buf_chg t where t.i_buf_chg_index = '1247';
+--delete from tbl_buf_chg t where t.i_buf_chg_index = '1248';
+--delete from tbl_buf_chg t where t.i_buf_chg_index = '1249';
+--delete from tbl_buf_chg t where t.i_buf_chg_index = '1250';
+
+--delete from tbl_ipc_dft_dsp t where t.txn_num = '9565';
+--delete from tbl_ipc_dft_dsp t where t.txn_num = '9566';
